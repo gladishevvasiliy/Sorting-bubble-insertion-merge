2ce12564c86c3654c1756ce42ada702898358211
gladishevvasiliy <gladishevvasiliy@gmail.com>
2016-02-18 08:12:12 +0300

    added code from Gdrive

---
+++ Sorting-bubble,insertion, merge/Sorting-bubble,insertion, merge.cpp
@@ -0,0 +1,118 @@
+// Sorting_2.02.16.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <tchar.h>
+#include "math.h"
+#include "conio.h"
+#include "iostream"
+#include "stdafx.h"
+
+using namespace std;
+
+
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	// A array
+	/*int n;
+	cout << "ENTER SIZE" << endl;
+	cin >> n;
+	int* a=new int[n];*/
+	int const n = 5;
+	int a[n];
+	cout << "ENTER ELEMENTS" << endl;
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	cout << "YOU A MASSIV" << endl;
+	for (int i = 0; i < n; i++) cout << a[i] << " ";
+	cout << endl;
+
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (a[j] > a[j + 1]) {
+				int b = a[j]; //change for elements
+				a[j] = a[j + 1];
+				a[j + 1] = b;
+			}
+		}
+	}
+
+	cout << "YOU FINISHED A MASSIV" << endl;
+	for (int i = 0; i < n; i++) cout << a[i] << " ";
+	cout << endl;
+	cout << endl;
+
+	// B array
+
+	/*int m, key, j;
+	cout << "ENTER SIZE" << endl;
+	cin >> m;
+	int* b = new int[m];*/
+
+	int const m = 5;
+	int b[m];
+	int key, j;
+
+	cout << "ENTER ELEMENTS" << endl;
+	for (int i = 0; i < m; i++) cin >> b[i];
+
+	cout << "YOU B MASSIV" << endl;
+	for (int i = 0; i < m; i++) cout << b[i] << " ";
+	cout << endl;
+
+	for (int i = 1; i < m; i++) {
+		key = b[i];
+		j = i - 1;
+		while (j >= 0 && b[j] > key) {
+			b[j + 1] = b[j];
+			j = j - 1;
+		}
+		b[j + 1] = key;
+	}
+
+	cout << "YOU FINISHED B MASSIV" << endl;
+	for (int i = 0; i < m; i++) cout << b[i] << " ";
+	cout << endl;
+	cout << endl;
+
+	//C ARRAY
+	int c[n + m];
+	//int *c = new int[n + m];
+
+
+
+	int i = 0;
+	int g = 0;
+	int l = 0;
+	while (i < n && g < m) {
+		if (a[i] < b[g]) {
+			c[l] = a[i];
+			i++;
+		}
+		else {
+			c[l] = b[g];
+			g++;
+		}
+		l++;
+	}
+	while (i < n) {
+		c[l] = a[i];
+		l++;
+		i++;
+	}
+
+	while (g < m) {
+		c[l] = b[g];
+		l++;
+		g++;
+	}
+	cout << "YOU FINISHED C MASSIV" << endl;
+	for (int i = 0; i < m + n; i++) cout << c[i] << " ";
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
+
+
